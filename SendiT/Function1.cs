c554401725fd8732d1c4fb51f362a77af519bcb9
Carlos C. Coelho <carloscamposcoelho@gmail.com>
2018-10-21 17:44:19 -0300

    Receiving json post request

--- SendiT/Function1.cs
+++ SendiT/Function1.cs
@@ -9,22 +9,23 @@ using Newtonsoft.Json;
 
 namespace SendiT
 {
-    public static class Function1
+    public static class Email
     {
-        [FunctionName("Function1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        [FunctionName("EmailQueue")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
+            HttpRequest req,
+            TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = new StreamReader(req.Body).ReadToEnd();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+            return data != null
+                ? (ActionResult)new OkObjectResult($"Received: {requestBody}")
+                : new BadRequestObjectResult("Please pass some json as a post request.");
         }
     }
 }
