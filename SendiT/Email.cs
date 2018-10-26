76d50f81ef27e08000104d9d95a1ae5c20baf3d0
Carlos C. Coelho <carloscamposcoelho@gmail.com>
2018-10-26 00:01:23 -0300

    #1 - put the incoming request into a queue of emails

--- SendiT/Email.cs
+++ SendiT/Email.cs
@@ -1,13 +1,12 @@
-
-using System.IO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using SendiT.Model;
 using SendiT.Util;
+using System.IO;
 
 namespace SendiT
 {
@@ -15,24 +14,37 @@ namespace SendiT
     {
         [FunctionName("EmailQueue")]
         public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
-            HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Queue("email-queue", Connection = "AzureWebJobsStorage")] out OutgoingEmail emailQueue,
+
             TraceWriter log)
         {
-            string modelErros;
-            log.Info("C# HTTP trigger function processed a request.");
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            var email = JsonConvert.DeserializeObject<OutgoingEmail>(requestBody);
-            
-            if (!ValidatorUtil.IsValid(email, out modelErros))
+            emailQueue = null;
+            try
             {
-                return new BadRequestObjectResult(modelErros);
-            }
+                string modelErros;
 
-            return email != null
-                ? (ActionResult)new OkObjectResult($"Received: {JsonConvert.SerializeObject(email)}")
-                : new BadRequestObjectResult("Please pass some json as a post request.");
+                log.Info("C# HTTP trigger function processed a request.");
+
+                string requestBody = new StreamReader(req.Body).ReadToEnd();
+                var email = JsonConvert.DeserializeObject<OutgoingEmail>(requestBody);
+
+                if (!ValidatorUtil.IsValid(email, out modelErros))
+                {
+                    return new BadRequestObjectResult(modelErros);
+                }
+
+                //Queue email request
+                emailQueue = email;
+
+                return new OkObjectResult("Request was successfully queued.");
+
+            }
+            catch (System.Exception ex)
+            {
+                log.Error("An error has ocurred.", ex);
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
